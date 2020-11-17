75ed2c4ed737e1da3c11e20b988f87f724f97476
Anamika <anamika.sawhney22@gmail.com>
2020-11-17 12:16:55 +0530

    Add project files.

---
+++ WebApiDemoNov/Controllers/ItemsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiDemoNov.Models;
+using System.Web.Http.Cors;
+
+namespace WebApiDemoNov.Controllers
+
+{
+
+   
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ItemsController : ApiController
+    {
+        private ItemDbContext db = new ItemDbContext();
+
+        // GET: api/Items
+        public IQueryable<Item> Getitems()
+        {
+            return db.items;
+        }
+
+        // GET: api/Items/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult GetItem(int id)
+        {
+            Item item = db.items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // PUT: api/Items/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutItem(int id, Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != item.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Items
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult PostItem(Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.items.Add(item);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = item.id }, item);
+        }
+
+        // DELETE: api/Items/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult DeleteItem(int id)
+        {
+            Item item = db.items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            db.items.Remove(item);
+            db.SaveChanges();
+
+            return Ok(item);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ItemExists(int id)
+        {
+            return db.items.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ WebApiDemoNov/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemoNov.Models
+{
+    public class Item
+    {
+        
+        public int id { get; set; }
+        public string name { get; set; }
+        public int qtyStock { get; set; }
+        public string description { get; set; }
+    }
+
+    public class ItemDbContext : DbContext
+    {
+        public DbSet<Item> items { get; set; }
+    }
+}
